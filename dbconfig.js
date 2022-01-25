df9b21e707897b3eb9a55a508b8cee71763dc167
juanframart2011 <juanframart2011@gmail.com>
2022-01-24 23:43:27 -0600

    connect config

---
+++ dbconfig.js
@@ -0,0 +1,11 @@
+const cinfig = {
+    user: 'sa',
+    password: '',
+    server: 'localhost',
+    database: 'DBTEST2',
+    options:{
+        trustedconnection: false,
+        enableArithAbort: true,
+        encrypt: false
+    }
+}
