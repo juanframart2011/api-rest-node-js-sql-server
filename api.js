a89439f7c617dfb2ef596f7f223b36ad3a972d23
juanframart2011 <juanframart2011@gmail.com>
2022-01-25 23:23:33 -0600

    post

--- api.js
+++ api.js
@@ -2,6 +2,7 @@ const dbcategory = require('./dbcategory');
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+const categoryModel = require('./category');
 
 var app = express();
 var router = express.Router();
@@ -11,8 +12,26 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use('/api', router);
 
+//lista de categorias
 router.route('/category').get((request,response) => {
     dbcategory.getCategory().then( result =>{
+
+        response.json(result[0]);
+    });
+});
+
+//Categoría id
+router.route('/category/:id').get((request,response) => {
+    dbcategory.getCategoryXId( request.params.id).then( result =>{
+
+        response.json(result[0]);
+    });
+});
+//Category save
+router.route('/category/save').post((request,response) => {
+
+    let categoryData = {...request.body};
+    dbcategory.insertCategory( categoryData ).then( result =>{
 
         response.json(result[0]);
     });
--- dbcategory.js
+++ dbcategory.js
@@ -13,7 +13,39 @@ async function getCategory(){
     }
 }
 
+async function getCategoryXId( id ){
+
+    try {
+        
+        let pool = await sql.connect( config );
+        let category = await pool.request()
+        .input('input_parameter', sql.Int, id)
+        .query( "SELECT * FROM tm_categoria where cat_id=@input_parameter" );
+        return category.recordsets;
+    } catch (error) {
+        console.warn( error );
+    }
+}
+
+async function insertCategory( category ){
+
+    try {
+        
+        let pool = await sql.connect( config );
+        let insertCategory = await pool.request()
+        .input('cat_id', sql.Int, category.cat_id)
+        .input('cat_nom', sql.Int, category.cat_nom)
+        .input('cat_obs', sql.Int, category.cat_obs)
+        .execute( "insert_category" );
+        return insertCategory.recordsets;
+    } catch (error) {
+        console.warn( error );
+    }
+}
+
 module.exports = {
 
-    getCategory : getCategory
+    getCategory : getCategory,
+    getCategoryXId: getCategoryXId,
+    insertCategory: insertCategory
 }
